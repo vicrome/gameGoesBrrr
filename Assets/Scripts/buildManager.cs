cea1f9d5148211e663c6d0bb8d110039d0944068
arturo <arturo.regi.alfa@gmail.com>
2020-12-12 23:46:32 +0100

    Build system v1.0

---
+++ Assets/Scripts/buildManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildManager : MonoBehaviour
+{
+    public GameObject foundation;
+    public buildSystem myBuildSystem;
+
+    // Update is called once per frame
+    public void BeginBuild()
+    {
+        if (!myBuildSystem.isBuilding)
+        {
+            Debug.Log("Has empezado a  construir");
+            myBuildSystem.NewBuild(foundation);
+        }
+    }
+}
