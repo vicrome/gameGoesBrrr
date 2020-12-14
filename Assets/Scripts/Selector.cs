ec0c3df0f16078778259660d05da15696bd615fa
arturo <arturo.regi.alfa@gmail.com>
2020-12-14 18:02:25 +0100

    Implement Units movement

---
+++ Assets/Scripts/Selector.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class Selector : MonoBehaviour
+{
+    [Header("For Raycast")]
+    public Camera cam;
+    public LayerMask mask;
+
+    [Header("Gameobject for selection box")]
+    public GameObject selectorBox;
+
+    [Header("Toggle Gizmos")]
+    public bool showGizmos;
+
+
+    private Vector3 movetoPos = Vector3.zero;
+
+    //[Header("Selection Box start/end points")]
+    private Vector3 startPoint;
+    private Vector3 endPoint;
+
+    //[Header("Selection Box Info")]
+    private RectangleF selectionRect;
+    private Vector3 rectCenter;
+    private Vector3 rectSize;
+    private Vector3 halfExtents;
+
+    //[Header("All of the currently Selected Units")]
+    private List<GameObject> selectedUnits = new List<GameObject>();
+
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPoint = DoRay();
+            selectorBox.SetActive(true);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            endPoint = DoRay();
+            HandleRectangle();
+            selectorBox.transform.position = rectCenter;
+            selectorBox.transform.localScale = rectSize + new Vector3(0f, 1f, 0f);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            selectorBox.SetActive(false);
+            endPoint = DoRay();
+            HandleRectangle();
+            SelectAllUnits();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            MoveAllUnits(DoRay());
+        }
+    }
+
+    private void HandleRectangle()
+    {
+        rectSize = startPoint - endPoint;
+        rectSize.x = Mathf.Abs(rectSize.x);
+        rectSize.y = Mathf.Abs(rectSize.y);
+        rectSize.z = Mathf.Abs(rectSize.z);
+
+        rectCenter = (startPoint + endPoint) * 0.5f;
+
+        halfExtents = rectSize * 0.5f;
+    }
+
+    private void ClearAllUnits()
+    {
+        for (int i = 0; i < selectedUnits.Count; i++)
+        {
+            Unit unit = selectedUnits[i].GetComponent<Unit>();
+            unit.DeselectUnit();
+        }
+
+        selectedUnits.Clear();
+    }
+
+    private void SelectAllUnits()
+    {
+        ClearAllUnits();
+
+        RaycastHit[] check = Physics.BoxCastAll(rectCenter, halfExtents, Vector3.up);
+
+        for (int i = 0; i < check.Length; i++)
+        {
+            if (check[i].collider.CompareTag("Unit"))
+            {
+                Unit unit = check[i].collider.GetComponent<Unit>();
+                unit.SelectUnit();
+                unit.CalculateOffset(rectCenter);
+                selectedUnits.Add(unit.gameObject);
+            }
+        }
+    }
+
+    private void MoveAllUnits(Vector3 _pos)
+    {
+        movetoPos = _pos;
+
+        for (int i = 0; i < selectedUnits.Count; i++)
+        {
+            Unit unit = selectedUnits[i].GetComponent<Unit>();
+            unit.MoveToSpot(_pos);
+        }
+    }
+
+    private Vector3 DoRay()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000f, mask))
+        {
+            if (hit.collider.CompareTag("Ground"))
+            {
+                return hit.point;
+            }
+        }
+
+        return Vector3.zero;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (showGizmos)
+        {
+            Gizmos.color = UnityEngine.Color.green;
+            Gizmos.DrawWireCube(rectCenter, rectSize);
+
+            Gizmos.color = UnityEngine.Color.yellow;
+            Gizmos.DrawWireSphere(startPoint, 0.5f);
+
+            Gizmos.color = UnityEngine.Color.red;
+            Gizmos.DrawWireSphere(endPoint, 0.5f);
+
+            Gizmos.color = UnityEngine.Color.cyan;
+            Gizmos.DrawSphere(movetoPos, 0.5f);
+
+            Gizmos.color = UnityEngine.Color.gray;
+            Gizmos.DrawSphere(rectCenter, 0.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Unit : MonoBehaviour
+{
+ 
+    private NavMeshAgent myAgent;
+    private GameObject selectorIcon;
+    private Vector3 startingPos;
+    private Vector3 offset;
+    public bool isSprite;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+        selectorIcon = transform.GetChild(0).gameObject;
+        selectorIcon.SetActive(false);
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+    public void SelectUnit()
+    {
+        selectorIcon.SetActive(true);
+    }
+
+    public void DeselectUnit()
+    {
+        selectorIcon.SetActive(false);
+        offset = Vector3.zero;
+    }
+
+    public void CalculateOffset(Vector3 _center)
+    {
+        Vector3 center = new Vector3(_center.x, transform.position.y, _center.z);
+        offset = center - transform.position;
+    }
+
+    public void MoveToSpot(Vector3 _pos)
+    {
+        Vector3 pos = new Vector3(_pos.x, transform.position.y, _pos.z);
+        Vector3 moveToPos = pos + offset;
+        myAgent.SetDestination(moveToPos);
+    }
+
+    public void MoveToSpot(Vector3 _pos, Vector3 _center)
+    {
+        Vector3 center = new Vector3(_center.x, transform.position.y, _center.z);
+        Vector3 pos = new Vector3(_pos.x, transform.position.y, _pos.z);
+
+        offset = center - transform.position;
+        Vector3 moveToPos = pos + offset;
+        myAgent.SetDestination(moveToPos);
+    }
+}
