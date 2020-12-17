a0ced5b7c301d197f714f991d42273c95646b13b
arturo <arturo.regi.alfa@gmail.com>
2020-12-17 21:53:37 +0100

    Implement Offline Scene with Local host and join

---
+++ Assets/Scripts/LobbyNetWorking/AutoHostClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class AutoHostClient : MonoBehaviour
+{
+
+    [SerializeField] NetworkManager networkManager;
+
+    private void Start()
+    {
+        if (Application.isBatchMode)
+        {
+            Debug.Log("Client connected");
+            networkManager.StartClient();
+        }
+        else
+        {
+            Debug.Log("Server starting");
+        }
+    }
+    
+    public void JoinLocal() 
+    {
+        networkManager.networkAddress = "localhost";
+        networkManager.StartClient();
+    }
+}
